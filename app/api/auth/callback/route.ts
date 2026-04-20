bed5323a3799b9aaf70737585eda7c3079c50002
Pablo David Encina Acevedo <Pablo.encina@klap.cl>
2026-04-19 23:48:14 -0400

    nuevo cambio 3

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,15 +1,19 @@
+import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
-import { NextResponse, type NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/dashboard";
 
   if (code) {
-    const supabase = createClient();
+    const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) return NextResponse.redirect(`${origin}${next}`);
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
   }
+
   return NextResponse.redirect(`${origin}/login?error=auth`);
 }
